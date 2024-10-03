2a0b4e8fcb39939f19f2e44dbcbb372ce7cfa27d
Felipe <fbc@cesar.school>
2024-10-03 12:19:00 -0300

    criaçao banco de dados de brinquedos

--- Projetos2/App_Projetos2/models.py
+++ Projetos2/App_Projetos2/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Brinquedo(models.Model):
+    nome = models.CharField(max_length=256)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    faixaetaria = models.CharField(max_length=256)
+    material = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.nome
