0f4b29c868bc8e481af8f0b2a60a60e09debd875
henrique-gueiros <riquewg@gmail.com>
2024-10-16 15:07:31 -0300

    Mudança do botão para ter acesso a aba de admin
    admin dentro do django pode visualizar as doações e adicionar novos brinquedos

--- Projetos2/App_Projetos2/admin.py
+++ Projetos2/App_Projetos2/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Brinquedo
+from .models import Brinquedo, Doacao
 # Register your models here.
 
 admin.site.register(Brinquedo)
+admin.site.register(Doacao)
