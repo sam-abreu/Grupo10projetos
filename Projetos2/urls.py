ebb7000d3356af2183aaf342a9aef5a16e3199d8
sam-abreu <168447703+sam-abreu@users.noreply.github.com>
2024-11-06 19:42:15 -0300

    Ajeitando as urls

--- Projetos2/urls.py
+++ Projetos2/urls.py
@@ -5,7 +5,7 @@ from App_Projetos2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.add_brinquedo, name='add_brinquedo'),
+    path('', views.user_login, name='login'),
     path('cadastro_user/', views.cadastro_user, name='cadastro_user'),
     path('home/', views.home, name='home'),
     path('doar/', views.add_doacao, name='doar'),
