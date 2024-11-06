ce3bc9db44a689bb6064d80c47a530b95c00e07c
henrique-gueiros <riquewg@gmail.com>
2024-11-05 22:05:36 -0300

    definindo a path login para já inicializar quando o site abrir

--- Projetos2/urls.py
+++ Projetos2/urls.py
@@ -5,9 +5,9 @@ from App_Projetos2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.user_login, name='login'),
+    path('', views.user_login, name='login'),
     path('cadastro_user/', views.cadastro_user, name='cadastro_user'),
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('doar/', views.add_doacao, name='doar'),
     path('comprar/', views.comprar_brinquedo, name='comprar_brinquedo'),
     path('add-brinquedo/', views.add_brinquedo, name='add_brinquedo'),
