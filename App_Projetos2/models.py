c21e3320578a4be32073972ea49bc934649384f0
henrique-gueiros <riquewg@gmail.com>
2024-11-22 13:44:38 -0300

    criando model para ser viável o acúmulo de pontos
    Criei a model "Profile", que é relacionada com a model padrão do django "User", e já está com o campo de pontos

--- App_Projetos2/models.py
+++ App_Projetos2/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Brinquedo(models.Model):
@@ -32,3 +33,9 @@ class Inscricao(models.Model):
     def __str__(self):
         return self.nome
     
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    pontos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Nome: {self.user.username} / Pontos: {self.pontos}"
